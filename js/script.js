130966589af1281c7706ee64b58b0393f3c4ec62
Adrián <adriancoroneltandil@gmail.com>
2023-10-26 20:44:33 -0300

    cambios en el css y en el javascript de landing page

--- js/script.js
+++ js/script.js
@@ -1,30 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-        var inicio = document.querySelector('a[href="index.html"]');
-        var quienesSomos = document.querySelector('a[href="#quiensomos"]');
-        var contacto = document.querySelector('a[href="#contacto"]');
-        var divQuienesSomos = document.querySelector('.quiensomos');
-        var divContacto = document.querySelector('.contacto');
-        var sectionHero = document.querySelector('.hero');
-        var sectionContenedorJuegos = document.querySelector('.contenedorjuegos');
+    var inicio = document.querySelector('a[href="index.html"]');
+    var quienesSomos = document.querySelector('a[href="#quiensomos"]');
+    var contacto = document.querySelector('a[href="#contacto"]');
+    var divQuienesSomos = document.querySelector('.quiensomos');
+    var divContacto = document.querySelector('.contacto');
+    var sectionHero = document.querySelector('.hero');
+    var sectionContenedorJuegos = document.querySelector('.contenedorjuegos');
 
-        inicio.addEventListener('click', function () {
-            divQuienesSomos.style.display = 'none';
-            divContacto.style.display = 'none';
-            sectionHero.style.display = 'block';
-            sectionContenedorJuegos.style.display = 'block';
-        });
+    // Ocultar los elementos al principio
+    divQuienesSomos.style.display = 'none';
+    divContacto.style.display = 'none';
 
-        quienesSomos.addEventListener('click', function () {
-            divQuienesSomos.style.display = 'block';
-            divContacto.style.display = 'none';
-            sectionHero.style.display = 'none';
-            sectionContenedorJuegos.style.display = 'none';
-        });
-
-        contacto.addEventListener('click', function () {
-            divQuienesSomos.style.display = 'none';
-            divContacto.style.display = 'block';
-            sectionHero.style.display = 'none';
-            sectionContenedorJuegos.style.display = 'none';
-        });
+    inicio.addEventListener('click', function () {
+        divQuienesSomos.style.display = 'none';
+        divContacto.style.display = 'none';
+        sectionHero.style.display = 'block';
+        sectionContenedorJuegos.style.display = 'block';
     });
+
+    quienesSomos.addEventListener('click', function () {
+        divQuienesSomos.style.display = 'block';
+        divContacto.style.display = 'none';
+        sectionHero.style.display = 'none';
+        sectionContenedorJuegos.style.display = 'none';
+    });
+
+    contacto.addEventListener('click', function () {
+        divQuienesSomos.style.display = 'none';
+        divContacto.style.display = 'block';
+        sectionHero.style.display = 'none';
+        sectionContenedorJuegos.style.display = 'none';
+    });
+});
